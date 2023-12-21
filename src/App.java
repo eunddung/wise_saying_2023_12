ebb9939555337817f8e01cd32e6c9ed563bcb160
eunddung <x72845221@gmail.com>
2023-12-21 09:10:08 +0900

    5단계목록시에 정보가 제대로 나오도록 수정

--- src/App.java
+++ src/App.java
@@ -11,10 +11,8 @@ public class App {
 
 	public void run() {
 		System.out.println("== 명언 앱 실행 ==");
-
 		int lastId = 0;
 		List<WiseSaying> wiseSayings = new ArrayList<>();
-
 		while (true) {
 			System.out.print("명령어 ) ");
 			String cmd = sc.nextLine().trim();
@@ -26,24 +24,29 @@ public class App {
 				String content = sc.nextLine().trim();
 				System.out.print("작가 : ");
 				String author = sc.nextLine().trim();
-
 				WiseSaying wiseSaying = new WiseSaying(id, content, author);
 				wiseSayings.add(wiseSaying);
 
 				System.out.printf("%d번 명언이 등록되었습니다.\n", id);
-				lastId++;// 마지막 번호 증가
+				lastId++;
 
 			} else if (cmd.equals("목록")) {
 				if (wiseSayings.size() == 0) {
 					System.out.println("등록 된 명언이 없어");
 				} else {
-					System.out.println("있던데???");
-					System.out.println("등록 된 명언 수 : " + wiseSayings.size());
+					System.out.println("번호  /  작가  /  명언  ");
+					System.out.println("=".repeat(30));
+
+					for (int i = wiseSayings.size() - 1; i >= 0; i--) {
+						WiseSaying ws = wiseSayings.get(i);
+
+						System.out.printf("%d  /  %s  /  %s\n", ws.getId(), ws.getAuthor(), ws.getContent());
+					}
+
 				}
 			} else {
 				System.out.println("존재하지 않는 명령어입니다");
 			}
 		}
-
 	}
 }
